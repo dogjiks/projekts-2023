2e3ddb85bd61124236a9a1df4a3be8673d383d48
dogjiks <144685923+dogjiks@users.noreply.github.com>
2023-10-09 15:26:50 +0300

    php
    php

--- proc.php
+++ proc.php
@@ -1,6 +1,18 @@
 <?php
 $l = $_POST["login"];
 $p = $_POST["password"];
+
+include('cfg.php');
+
+
+$result = mysqli_query("SELECT * FROM users WHERE login=$l");
+if (mysqli_num_rows($result) > 0){
+    echo "GOOD!!!";
+}
+else{
+    echo "BYE BYE!!!"
+}
+
 echo "$l, HELLO, $p";
 
 ?>
